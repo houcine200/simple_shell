8c3a31ff8c6a1a43b6c1176fa18694b5a8724ecf
Houcine Walaq <houcine.walaq@gmail.com>
2023-08-21 12:46:23 -0700

    add atoi func

--- more_func.c
+++ more_func.c
@@ -53,3 +53,26 @@ char *_getenv(char *name)
 	}
 	return NULL;
 }
+/**
+ * _atoi - converts a string to an integer.
+ * @s: the string to convert
+ * Return: the converted string.
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int num = 0;
+
+	for (; s[0] != '\0'; s++)
+	{
+		if (s[0] == '-')
+			sign = -sign;
+
+		else if (s[0] >= '0' && s[0] <= '9')
+			num = (num * 10) + (s[0] - '0');
+
+		else if (num > 0)
+			break;
+	}
+	return (num * sign);
+}
