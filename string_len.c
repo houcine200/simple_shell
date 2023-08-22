352ef6fbbc341d87ab4afeb401732b7a2467b3c4
Elimali hamza <elimal.hamza@gmail.com>
2023-08-22 15:39:51 -0700

    compiler

--- string_len.c
+++ string_len.c
@@ -53,7 +53,8 @@ char *_strcat(char *dest, const char *src)
 
     return saved_dest;
 }
-int _strncmp(const char *s1, const char *s2, size_t n) {
+int _strncmp(const char *s1, const char *s2, size_t n)
+{
     while (n > 0 && *s1 && *s1 == *s2) {
         s1++;
         s2++;
