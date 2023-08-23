1c9109275f9ef94f4f68491ea6cfea163be4d1d3
Elimali hamza <elimal.hamza@gmail.com>
2023-08-23 03:59:54 -0700

    env_func

--- env_func.c
+++ env_func.c
@@ -57,40 +57,40 @@ void execute_unsetenv(char **args)
 
 /**
  * _setenv - Set a new environment variable or modify an existing one
- * @name: The name of the variable to set
- * @value: The value to set for the variable
- * @overwrite: Flag to determine
+ * @var_name: The name of the variable to set
+ * @var_value: The value to set for the variable
+ * @flag_overwrite: Flag to determine
  * if overwriting an existing variable is allowed
  * Return: 0 on success, -1 on failure
  */
-int _setenv(const char *name, const char *value, int overwrite)
+int _setenv(const char *var_name, const char *var_value, int flag_overwrite)
 {
 	char *new_var;
 	int i;
 
-	if (!name || !value)
+	if (!var_name || !var_value)
 		return (-1);
 
 	for (i = 0; environ[i]; i++)
 	{
-		if (_strncmp(environ[i], name, _strlen(name)) == 0)
+		if (_strncmp(environ[i], var_name, _strlen(var_name)) == 0)
 		{
-			if (overwrite)
+			if (flar_overwrite)
 			{
-				new_var = malloc(_strlen(name) + _strlen(value) + 2);
-				_strcpy(new_var, name);
+				new_var = malloc(_strlen(var_name) + _strlen(var_value) + 2);
+				_strcpy(new_var, var_name);
 				_strcat(new_var, "=");
-				_strcat(new_var, value);
+				_strcat(new_var, var_value);
 				environ[i] = new_var;
 				return (0);
 			}
 			return (0);
 		}
 	}
-	new_var = malloc(_strlen(name) + _strlen(value) + 2);
-	_strcpy(new_var, name);
+	new_var = malloc(_strlen(var_name) + _strlen(var_value) + 2);
+	_strcpy(new_var, var_name);
 	_strcat(new_var, "=");
-	_strcat(new_var, value);
+	_strcat(new_var, var_value);
 	environ[i] = new_var;
 	environ[i + 1] = NULL;
 	return (0);
@@ -98,20 +98,20 @@ int _setenv(const char *name, const char *value, int overwrite)
 
 /**
  * _unsetenv - Remove an environment variable
- * @name: The name of the variable to remove
+ * @var_name: The name of the variable to remove
  * Return: 0 on success, -1 on failure
  */
 
-int _unsetenv(const char *name)
+int _unsetenv(const char *var_name)
 {
 	int i, j = 0;
 
-	if (!name || name[0] == '\0' || strchr(name, '=') != NULL)
+	if (!var_name || var_name[0] == '\0' || strchr(var_name, '=') != NULL)
 		return (-1);
 
 	for (i = 0; environ[i]; i++)
 	{
-		if (_strncmp(environ[i], name, _strlen(name)) == 0
+		if (_strncmp(environ[i], var_name, _strlen(var_name)) == 0
 				&& environ[i][_strlen(name)] == '=')
 		{
 			free(environ[i]);
