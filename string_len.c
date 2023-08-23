bcbc92d034da0c9e40f1c6581860a9c03ba5eb22
Houcine Walaq <houcine.walaq@gmail.com>
2023-08-23 04:31:56 -0700

    modify strncmp

--- string_len.c
+++ string_len.c
@@ -92,15 +92,15 @@ char *_strcat(char *dest, const char *src)
 
 int _strncmp(const char *s1, const char *s2, size_t n)
 {
-	while (n > 0 && *s1 && *s1 == *s2)
-	{
-		s1++;
-		s2++;
-		n--;
-	}
+	int i, j;
+
+	for (i = 0, j = 0; n > 0 && s1[i] && s1[i] == s2[j]; i++, j++, n--)
+		;
+
 	if (n == 0)
 	{
-		return (0);
+		return 0;
 	}
-	return (*(unsigned char *)s1 - *(unsigned char *)s2);
+
+	return (*(unsigned char *)&s1[i] - *(unsigned char *)&s2[j]);
 }
